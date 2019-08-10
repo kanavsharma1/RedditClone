a7bfee8164423dd2b69a8f3b33ff40fb02693798
kanav sharma <kanavsharma1@outlook.com>
2019-08-10 20:08:17 +0530

    Added entities to the domain package

---
+++ src/main/java/redditClone/Reddit/domain/Comment.java
@@ -0,0 +1,19 @@
+package redditClone.Reddit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+private long id;
+private String body;
+
+
+}
---
+++ src/main/java/redditClone/Reddit/domain/Link.java
@@ -0,0 +1,20 @@
+package redditClone.Reddit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String url;
+}
